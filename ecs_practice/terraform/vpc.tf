22ca88359beeadd158f6c56e2517f2f9dd839e58
yuki hara <hara.fuku.3194@gmail.com>
2025-01-12 12:38:03 +0900

    tmp

--- ecs_practice/terraform/vpc.tf
+++ ecs_practice/terraform/vpc.tf
@@ -1,26 +1,44 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+# resource "aws_vpc" "main" {
+#   cidr_block = "10.0.0.0/16"
 
-  tags = {
-    Name = "main"
-  }
-}
+#   tags = {
+#     Name = "main"
+#   }
+# }
 
-resource "aws_subnet" "this" {
-  for_each = { for subnet in local.subnets : subnet.name => subnet }
-  vpc_id     = aws_vpc.main.id
-  cidr_block = each.value.cidr
-  availability_zone = each.value.az
+# resource "aws_subnet" "this" {
+#   for_each = { for subnet in local.subnets : subnet.name => subnet }
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = each.value.cidr
+#   availability_zone = each.value.az
 
-  tags = {
-    Name = each.value.name
-  }
-}
+#   tags = {
+#     Name = each.value.name
+#   }
+# }
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
+# resource "aws_internet_gateway" "main" {
+#   vpc_id = aws_vpc.main.id
 
-  tags = {
-    Name = "main"
-  }
-}
+#   tags = {
+#     Name = "main"
+#   }
+# }
+
+# resource "aws_route_table" "name" {
+
+# }
+
+# resource "aws_se" "name" {
+
+# }
+
+# resource "aws_security_group" "allow_tls" {
+#   vpc_id      = aws_vpc.main.id
+#   name        = "allow_tls"
+#   description = "Allow TLS inbound traffic and all outbound traffic"
+
+#   tags = {
+#     Name = "allow_tls"
+#   }
+# }
