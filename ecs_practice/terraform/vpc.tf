28548e54ccbdfec16e01d6104015670808ea537e
yuki hara <hara.fuku.3194@gmail.com>
2025-01-13 17:38:38 +0900

    add:内部alb用のsg追加

--- ecs_practice/terraform/vpc.tf
+++ ecs_practice/terraform/vpc.tf
@@ -77,6 +77,14 @@ resource "aws_security_group" "this" {
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "internal" {
+  security_group_id = aws_security_group.this["ingress"].id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "tcp"
+  from_port   = 80
+  to_port = 80
+}
+
 resource "aws_vpc_security_group_egress_rule" "vpc_endpoint" {
   security_group_id = aws_security_group.this["vpc_endpoint"].id
   cidr_ipv4 = "0.0.0.0/0"
