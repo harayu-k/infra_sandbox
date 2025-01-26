362809a68abeb40c98d83f25f9ae11aa61f99727
yuki hara <hara.fuku.3194@gmail.com>
2025-01-26 18:06:56 +0900

    fix:sg config

--- ecs_practice/terraform/vpc.tf
+++ ecs_practice/terraform/vpc.tf
@@ -113,18 +113,18 @@ resource "aws_vpc_security_group_egress_rule" "frontend" {
   cidr_ipv4 = "0.0.0.0/0"
 }
 
-# resource "aws_vpc_security_group_ingress_rule" "frontend" {
-#   security_group_id = aws_security_group.this["frontend"].id
-#   referenced_security_group_id = aws_security_group.this["ingress"].id
-#   ip_protocol = "-1"
-#   cidr_ipv4 = "0.0.0.0/0"
-# }
+resource "aws_vpc_security_group_ingress_rule" "frontend" {
+  security_group_id = aws_security_group.this["frontend"].id
+  referenced_security_group_id = aws_security_group.this["ingress"].id
+  ip_protocol = "-1"
+}
 
 resource "aws_vpc_security_group_ingress_rule" "ingress" {
   security_group_id = aws_security_group.this["ingress"].id
   ip_protocol = "tcp"
   from_port = "80"
   to_port = "80"
+  cidr_ipv4 = "0.0.0.0/0"
 }
 
 # ############################
